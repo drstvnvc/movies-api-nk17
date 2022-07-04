7b22b83380a86ba04981c6172793562324ce49c3
Drago Stevanovic <drago@sounder.fm>
2022-07-04 21:31:13 +0200

    Create resource route

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/movies', [MovieController::class, 'index']);
-Route::get('/movies/{movie}', [MovieController::class, 'show']);
-Route::post('/movies', [MovieController::class, 'store']);
-Route::put('/movies/{movie}', [MovieController::class, 'update']);
-Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
+// Route::get('/movies', [MovieController::class, 'index']);
+// Route::get('/movies/{movie}', [MovieController::class, 'show']);
+// Route::post('/movies', [MovieController::class, 'store']);
+// Route::put('/movies/{movie}', [MovieController::class, 'update']);
+// Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
+Route::apiResource('movies', MovieController::class);
