e536c555c96a40e30cd6b896c85460226879c73d
Drago Stevanovic <drago@sounder.fm>
2022-07-06 20:38:54 +0200

    JWT Authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,4 +25,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::post('/movies', [MovieController::class, 'store']);
 // Route::put('/movies/{movie}', [MovieController::class, 'update']);
 // Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
-Route::apiResource('movies', MovieController::class);
+Route::apiResource('movies', MovieController::class)->middleware('auth');
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/profile', [AuthController::class, 'getMyProfile'])->middleware('auth');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
